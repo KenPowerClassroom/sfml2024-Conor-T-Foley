b4b8f0889f2c28c4904d541d4785cbb698f09102
Conor Foley <C00301460@setu.ie>
2024-11-15 15:41:04 +0000

    Initial Commit: Commented the code

--- 16_SFML_Games/minesweeper.cpp
+++ 16_SFML_Games/minesweeper.cpp
@@ -8,14 +8,21 @@ int minesweeper()
 
     RenderWindow app(VideoMode(400, 400), "Minesweeper!");
 
+    //This is a magic number that seems to be responsilbe for the squares on the grid being 32x32?
     int w=32;
-    int grid[12][12];
-    int sgrid[12][12]; //for showing
 
-    Texture t;
+    //Non descriptive names
+    int grid[12][12]; //Only 10x10 of the 12x12 grid is being drawn, this is the underlying grid
+    int sgrid[12][12]; //This is the grid shown to the player while the game runs
+
+    Texture t; //There are many letters in this code, this doesnt need to be another one
     t.loadFromFile("images/minesweeper/tiles.jpg");
-    Sprite s(t);
+    Sprite s(t); //Rename with a better name
 
+
+    //This loop is randomly allocating where the mines go
+    //Each square has a 1 in 5 chnce to get a mine
+    //The number 9 repsesents the mines
     for (int i=1;i<=10;i++)
      for (int j=1;j<=10;j++)
       {
@@ -24,6 +31,10 @@ int minesweeper()
         else grid[i][j]=0;
       }
 
+
+    //This loop is going through each tile on the grid
+    //Checking if that tile is a mine
+    //Then checking how many surrounding square have mines and keeping a count
     for (int i=1;i<=10;i++)
      for (int j=1;j<=10;j++)
       {
@@ -43,27 +54,37 @@ int minesweeper()
     while (app.isOpen())
     {
         Vector2i pos = Mouse::getPosition(app);
+        //These keep a track of which square the mouse is in 
         int x = pos.x/w;
         int y = pos.y/w;
 
         Event e;
+
+        //As long as the program is listening for events....
         while (app.pollEvent(e))
         {
+            //If that event is clicking the X, close the app
             if (e.type == Event::Closed)
                 app.close();
-
+            //If the event is a mouse click..
             if (e.type == Event::MouseButtonPressed)
+                //If the event is a left click reveal the square the mouse is in 
               if (e.key.code == Mouse::Left) sgrid[x][y]=grid[x][y];
+                //Else if the mouse click is a right click, place a flag
               else if (e.key.code == Mouse::Right) sgrid[x][y]=11;
         }
-
+        //Clears the screen before drawing the game
         app.clear(Color::White);
 
+        //Iterates through each tile in the grid
         for (int i=1;i<=10;i++)
          for (int j=1;j<=10;j++)
           {
+             //If that tile has a mine on it reveal the whole board?
            if (sgrid[x][y]==9) sgrid[i][j]=grid[i][j];
+           //Sets each tile to have the correct texture from the spritesheet, mine blank number etc..
            s.setTextureRect(IntRect(sgrid[i][j]*w,0,w,w));
+           //Places the sprites on the correct parts of the grid
            s.setPosition(i*w, j*w);
            app.draw(s);
           }
